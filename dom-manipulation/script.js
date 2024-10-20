6789824c07f30edfe977f172263ad0095557de80
Patriciakev-ctrl <tchinguemakamtepatriciakevine@gmail.com>
2024-10-20 22:48:01 +0100

    complete the second task

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -77,7 +77,16 @@ document.addEventListener("DOMContentLoaded",()=>{
     });
 })
 
-
+function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
 
 
 //createAddQuoteForm();
