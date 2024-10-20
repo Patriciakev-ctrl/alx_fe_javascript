bf092fc3ea39983549b2f9a9efdb90daf7ccb4d3
Patriciakev-ctrl <tchinguemakamtepatriciakevine@gmail.com>
2024-10-20 22:42:41 +0100

    complete the second task

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,17 +1,28 @@
 const quoteDisplay = document.getElementById("quoteDisplay");
 const newQuote = document.getElementById("newQuote");
-const arrayQuote = [{
-    quote: "",
-    category: ""
-}];
 
-function showRandomQuote() {
+
+function getItems() {
+    const quotes = JSON.parse(localStorage.getItem("quote")) || [{quote: "Je suis le CHEMIN", category:"biblique"}];
+    return quotes;
+}
+
+function addItem(item) {
+    let items = [{quote: "Je suis le CHEMIN", category:"biblique"}];
+    items = getItems();
+    items.push();
+    localStorage.setItem("quote",JSON.stringify(items));
+}
+
+
+function showRandomQuote(arrayQuote) {
     let randomNumber = 99*(Math.random());
     return arrayQuote[randomNumber];
 }
 
 function createAddQuoteForm() {
     const form = document.createElement("form");
+
     
     const inputForText = document.createElement("input");
     inputForText.setAttribute("type","text");
@@ -28,16 +39,45 @@ function createAddQuoteForm() {
     button.innerHTML = "Add Quote";
     button.setAttribute("onclick","addQuote()");
     
-    form.appendChild(labelForText);
+
     form.appendChild(inputForText);
-    form.appendChild(labelForCategory);
     form.appendChild(inputForCategory);
     form.appendChild(button);
     document.body.appendChild(form);
 
+        button.addEventListener("click",()=>{
+            addItem({quote: inputForText.value, category: inputForCategory.value});
+        });
+    
+    
+
 
 }
+document.addEventListener("DOMContentLoaded",()=>{
 
-newQuote.addEventListener("click", ()=>{});
+    newQuote.addEventListener("click", ()=>{
+        createAddQuoteForm();
+    });
+
+    const quotes = getItems();
+    quotes.forEach(element => {
+        const ul = document.createElement("ul");
+        const newQuote = document.createElement("li");
+            newQuote.innerHTML = "<i>"+element.quote+"</i>"+"<br/>"+"<em>"+element.category+"</em>";
+            button =document.createElement("button");
+            button.textContent = "Remove";
+            button.classList.add("remove-btn");
+            button.addEventListener("click", ()=>{
+                ul.removeChild(newQuote);
+            });
+            newQuote.appendChild(button);
+            ul.appendChild(newQuote);
+            quoteDisplay.appendChild(ul);
+            quoteDisplay.style.display = "block";
+    });
+})
+
+
+
 
 //createAddQuoteForm();
